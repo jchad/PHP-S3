a5c37b3483c6953e6395b63482f20d8ddda12ac4
Jordan CHADUIRON <jordan.chaduiron@etu.univ-lyon1.fr>
2016-09-21 16:22:34 +0200

    Inital commit

---
+++ castingV1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<?php
+	require_once("connexion.php");
+	$bdd= connexion_db();
+	$req=$bdd->query('SELECT Titre, Année, Score, Votes FROM Movie');
+	$data=$req->fetchAll(PDO::FETCH_ASSOC);
+	$count=$req->rowCount();
+ ?>
+<HTML>
+	<HEAD>
+		<title>Listes des films référencés</title>
+		<META charset='utf-8'>
+		<link rel="stylesheet" type="text/css" href="filmV1.css">
+	</HEAD>
+	<BODY>
+		<h1>Liste des Films</h1>
+		<?php
+			echo '<p>'.$count. ' film(s) trouvé(s) dans la base de données</p>';
+			echo '<table><tr><th>Titre</th><th>Année</th><th>Score</th><th>Votes</th>';
+			foreach($data as $ligne)
+				echo '<tr><td>'.$ligne['Titre'].'</td><td>'.$ligne['Année'].'</td><td>'.$ligne['Score'].'</td><td>'.$ligne['Votes'].'</td><tr>';
+			echo '</table>';
+		 ?>
+
+
+
+	</BODY>
+</HTML>

d3908b3d3657a89a00b19ea9faad6abb367bcc61
Jordan CHADUIRON <jordan.chaduiron@etu.univ-lyon1.fr>
2016-10-05 16:20:09 +0200

    TD 3 - Corection Lien

--- TD3/Views/films.php
+++ TD3/Views/films.php
@@ -5,7 +5,7 @@
   echo '<p>'.$count. ' film(s) trouvé(s) dans la base de données</p>';
   echo '<table><tr><th>Titre</th><th>Année</th><th>Score</th><th>Votes</th><th></th></tr>';
   foreach($results as $ligne)
-    echo '<tr><td>'.$ligne['Titre'].'</td><td>'.$ligne['Année'].'</td><td>'.$ligne['Score'].'</td><td>'.$ligne['Votes'].'</td><td><a href="http://localhost/php/PHP-S3/TD3/index.php?movieid='.$ligne['MovieID'].'">détails</a></td></tr>';
+    echo '<tr><td>'.$ligne['Titre'].'</td><td>'.$ligne['Année'].'</td><td>'.$ligne['Score'].'</td><td>'.$ligne['Votes'].'</td><td><a href="./index.php?movieid='.$ligne['MovieID'].'">détails</a></td></tr>';
   echo '</table>';
   $contenu=ob_get_clean();
   require("Views/menu.php");
--- TD3/Views/menu.php
+++ TD3/Views/menu.php
@@ -1,5 +1,5 @@
 <?php
   ob_start();
-  echo '<nav><a href="http://localhost/php/PHP-S3/TD3/index.php">Accueil</a></nav>';
+  echo '<nav><a href="./index.php">Accueil</a></nav>';
   $menu = ob_get_clean();
  ?>
